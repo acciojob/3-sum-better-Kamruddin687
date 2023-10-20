194a1b8d4ce10cf715b3be531853b2fb79fc8834
MD KAMRUDDIN ANSARI <57359057+Kamruddin687@users.noreply.github.com>
2023-10-21 02:14:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function threeSum(nums, target) {
+  nums.sort((a, b) => a - b); // Sort the array in ascending order.
+  let closestSum = nums[0] + nums[1] + nums[2]; // Initialize closestSum with the sum of the first three elements.
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const currentSum = nums[i] + nums[left] + nums[right];
+
+      if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+        closestSum = currentSum;
+      }
+
+      if (currentSum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return closestSum;
 }
 
-module.exports = threeSum;
+// Example usage
+const S = [-1, 2, 1, -4];
+const target = 1;
+const closest = threeSum(S, target);
+console.log("The sum closest to the target is:", closest);
